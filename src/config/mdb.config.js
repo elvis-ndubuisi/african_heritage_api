d217c1e22c806c90b6f13788f49305dcdb2eb146
SimplyElvis <provictor.ie@gmail.com>
2022-05-14 15:25:36 +0100

    Added base required files.

---
+++ src/config/mdb.config.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+// const localuri = "mongodb://localhost:27017/africanProverbDatabase";
+
+const connectMongo = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
+  } catch (err) {
+    console.log(err.red.bold);
+    process.exit(1);
+  }
+};
+
+module.exports = connectMongo;
