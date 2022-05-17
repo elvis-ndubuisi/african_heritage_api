79963747771cd1d0aa943a1ad1fc28fbdc432a83
Elvis <provictor.ie@gmail.com>
2022-05-17 10:42:38 +0100

    CORS init.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const colors = require("colors");
 const connectMongo = require("./config/mdb.config");
+const cors = require("cors");
 const dotenv = require("dotenv").config();
 const express = require("express");
 const helmet = require("helmet");
@@ -8,6 +9,7 @@ const app = express();
 const PORT = process.env.port || 8000;
 
 app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
