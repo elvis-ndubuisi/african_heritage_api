c8b4a2a07179e4b753fbd87266de9aed65f4c63e
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2022-06-21 01:15:09 +0100

    bug fixes (#19)
    * Cloud ready (#12)
    
    * Adage api.
    
    * cron jobs.
    
    * inst cookie-parser
    
    * token in cookie
    
    * Fix expire time.
    
    * adage api
    
    * adage api.
    
    * CORS issue fix -- at last.
    
    * redis-cloud setup
    
    * init adage fix.
    
    * fixing railway deployment.
    
    * added railway Procfile.
    
    * fixing mongo railway error
    
    * setup railway hosting.
    
    * added pagination to getAdage route.
    
    * trycatch adageOTday
    
    * auth token fix.
    
    * bug fixes

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 <h1 align="center">African Adage</h1>
 
 The African Adage API
+The African Heritage API is a JSON RESTfull API that returns African proverbs, stories, and more
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -40,14 +40,9 @@ const registerContributor = async (req, res, next) => {
     const accessToken = await signAccessToken(resp.id);
     const refreshToken = await signRefreshToken(resp.id);
 
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      maxAge: 86400,
-      sameSite: "None",
-      secure: true,
-    });
     res.send({
       accessToken,
+      refreshToken,
       name: resp.name,
       country: resp.country,
       gender: resp.gender,
@@ -79,14 +74,10 @@ const loginContributor = async (req, res, next) => {
 
     const accessToken = await signAccessToken(foundContributor.id);
     const refreshToken = await signRefreshToken(foundContributor.id);
-    res.cookie("refreshToken", refreshToken, {
-      httpOnly: true,
-      maxAge: 86400,
-      sameSite: "None",
-      secure: true,
-    });
+
     res.send({
       accessToken,
+      refreshToken,
       name: foundContributor.name,
       email: foundContributor.email,
       country: foundContributor.country,
@@ -102,19 +93,14 @@ const loginContributor = async (req, res, next) => {
 // @route   public
 const generateNewRefreshToken = async (req, res, next) => {
   try {
-    const { refreshToken } = req.cookies;
+    const { refreshToken } = req.body;
     if (!refreshToken) throw createErr.BadRequest();
-
     const userId = await verifyRefreshToken(refreshToken);
+
     const accessToken = await signAccessToken(userId);
     const refT = await signRefreshToken(userId);
-    res.cookie("refreshToken", refT, {
-      httpOnly: true,
-      maxAge: 86400,
-      sameSite: "None",
-      secure: true,
-    });
-    res.send({ accessToken });
+    // set new refresh token.
+    res.send({ accessToken, refreshToken: refT });
   } catch (err) {
     next(err);
   }
@@ -125,15 +111,10 @@ const generateNewRefreshToken = async (req, res, next) => {
 // @route   public
 const logout = async (req, res, next) => {
   try {
-    const { refreshToken } = req.cookies;
+    const { refreshToken } = req.body;
     if (!refreshToken) throw createErr.BadRequest();
     const userId = await verifyRefreshToken(refreshToken);
 
-    res.clearCookie("refreshToken", {
-      httpOnly: true,
-      sameSite: "None",
-      secure: true,
-    });
     const resp = await redisClient.DEL(userId);
     if (resp) {
       return res.sendStatus(204);
