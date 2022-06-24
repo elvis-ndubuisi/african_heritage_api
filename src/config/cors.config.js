f0afa5b67746b5ed590005fcf68bceb50091b216
Elvis <provictor.ie@gmail.com>
2022-06-24 15:21:43 +0100

    fix

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,8 +1,22 @@
+const allowOrigins = ["http://localhost:8080", "http://localhost:5000"];
+
 const contributorCORS = {
   origin: "http://localhost:8080",
   methods: ["GET", "POST", "PATCH", "DELETE"],
   optionsSuccessStatus: 200,
   credentials: true,
+};
+
+const corsOptionsDelegate = (req, fn) => {
+  let corsOptions;
+  if (allowOrigins.indexOf(req.header("Origin")) !== -1) {
+    // reflect the requested origin in the CORS response.
+    corsOptions = { origin: true };
+  } else {
+    // disable CORS for this request.
+    corsOptions = { origin: false };
+  }
+  fn(null, corsOptions);
 };
 
 const apiCORS = {
