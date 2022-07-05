d11967f3c280aae2c9e39c18801e7893a4508cb8
Elvis Ndubuisi Ike <provictor.ie@gmail.com>
2022-07-05 10:09:43 +0100

    update cors.config file

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,5 +1,6 @@
 const contributorCORS = {
-  origin: "http://localhost:8080",
+//   origin: "http://localhost:8080",
+  origin: "https://africanheritage.vercel.app",
   methods: ["GET", "POST", "PATCH", "DELETE"],
   optionsSuccessStatus: 200,
   credentials: true,
